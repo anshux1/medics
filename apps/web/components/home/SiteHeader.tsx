5fbb59058de79732413935d24b9f92251b5ca21e
Anshu kumar singh <anshukrsingh11@gmail.com>
2025-01-09 20:47:46 +0530

    auth config

--- apps/web/components/home/SiteHeader.tsx
+++ apps/web/components/home/SiteHeader.tsx
@@ -4,8 +4,8 @@ import ThemeToggle from "../ThemeToggle";
 import { auth, signIn, signOut } from "@/lib/auth";
 
 export async function SiteHeader() {
-  const session = await auth()
-  console.log(session)
+  const session = await auth();
+  console.log(session);
   return (
     <header className="container sticky top-0 backdrop-blur-xl z-40 flex h-16 items-center justify-between py-8 px-6">
       <div className="flex items-center space-x-2">
@@ -21,21 +21,23 @@ export async function SiteHeader() {
         {session ? (
           <form
             action={async () => {
-              "use server"
-              await signOut()
+              "use server";
+              await signOut();
             }}
           >
             <Button type="submit">Sign Out</Button>
           </form>
         ) : (
-          <form action={async () => {
-            "use server"
-            try {
-              await signIn("google")
-            } catch (error) {
-              throw error // Rethrow all other errors
-            }
-          }}>
+          <form
+            action={async () => {
+              "use server";
+              try {
+                await signIn("google");
+              } catch (error) {
+                throw error; // Rethrow all other errors
+              }
+            }}
+          >
             <Button variant="outline">Sign in</Button>
           </form>
         )}
--- apps/web/lib/auth.ts
+++ apps/web/lib/auth.ts
@@ -1,8 +1,8 @@
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import NextAuth from "next-auth"
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import NextAuth from "next-auth";
 
-import authConfig from "@/lib/auth.config"
-import { prisma } from "@workspace/database"
+import authConfig from "@/lib/auth.config";
+import { prisma } from "@workspace/database";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
   pages: {
@@ -11,18 +11,23 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
   },
   events: {
     async linkAccount({ user }) {
-
+      await prisma.user.update({
+        where: { id: user.id },
+        data: {
+          emailVerified: new Date(),
+        },
+      })
     },
   },
   callbacks: {
     async session({ session, token }) {
       if (token.sub) {
-        session.user.id = token.sub
+        session.user.id = token.sub;
       }
-      return session
+      return session;
     },
   },
   adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt" },
   ...authConfig,
-})
+});
